512699ed44c5f8afa89de2468fee4e351ae7d8a2
Kah Ho <sweekahho@gmail.com>
2025-08-20 21:30:56 +0800

    Rename Duke to Mochi and modify output text

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,12 @@
+public class Mochi {
+    public static void main(String[] args) {
+        System.out.println("""
+                ____________________________________________________________
+                 Hello! I'm Mochi
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """);
+    }
+}
