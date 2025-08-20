863f64f2841425acbffe0ed82d4d129712a33772
Kah Ho <sweekahho@gmail.com>
2025-08-21 02:54:34 +0800

    Create Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    protected final String description;
+    protected boolean completed;
+
+    public Task(String desc) {
+        this.description = desc;
+        this.completed = false;
+    }
+
+    public void mark() {
+        this.completed = true;
+        System.out.printf("""
+                ____________________________________________________________
+                 Nice! I've marked this task as done:
+                   [X] %s
+                ____________________________________________________________
+                """, description);
+    }
+
+    public void unmark() {
+        this.completed = false;
+        System.out.printf("""
+                ____________________________________________________________
+                 OK, I've marked this task as not done yet:
+                   [ ] %s
+                ____________________________________________________________
+                """, description);
+    }
+
+    @Override
+    public String toString() {
+        char status = completed ? 'x' : ' ';
+        return String.format("[%c] %s", status, description);
+    }
+}
