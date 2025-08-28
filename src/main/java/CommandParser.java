d759376c31e833e2ee16be49f3a4b83d4e3907e2
Kah Ho <sweekahho@gmail.com>
2025-08-29 05:16:40 +0800

    Create CommandParser, TaskList and UI as OOP classes to organize functions

---
+++ src/main/java/CommandParser.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class CommandParser {
+    private final Scanner scan;
+    private String command;
+    private String parameters;
+    private boolean running;
+
+    private final String[] commands = new String[] {
+            "bye", "list", "mark", "unmark", "todo", "deadline", "event", "delete", "help"
+    };
+
+    public CommandParser() {
+        this.scan = new Scanner(System.in);
+        this.running = true;
+    }
+
+    public void read() {
+        String s = this.scan.nextLine();
+        this.parameters = s;
+        if (s.startsWith("bye")) {
+            this.running = false;
+        } else {
+            for (String word : commands) {
+                if (s.startsWith(word)) {
+                    this.command = word;
+                    return;
+                }
+            }
+        }
+    }
+
+    public boolean is(String s) {
+        return s.equals(this.command);
+    }
+
+    public boolean running() {
+        return this.running;
+    }
+
+    public int markCommand(int listSize) throws MarkingException {
+        String task = this.parameters.substring(4).trim();
+        int taskNo;
+        try {
+            taskNo = Integer.parseInt(task);
+        } catch (Exception e) {
+            throw new MarkingException(task, listSize);
+        }
+        if (taskNo > listSize || taskNo < 1) {
+            throw new MarkingException(task, listSize);
+        }
+        return taskNo;
+    }
+
+    public int unmarkCommand(int listSize) throws MarkingException {
+        String task = this.parameters.substring(6).trim();
+        int taskNo;
+        try {
+            taskNo = Integer.parseInt(task);
+        } catch (Exception e) {
+            throw new MarkingException(task, listSize);
+        }
+        if (listSize == 0 || taskNo > listSize) {
+            throw new MarkingException(task, listSize);
+        }
+        return taskNo;
+    }
+
+    public Task todoCommand() throws ToDoException {
+        String task = this.parameters.substring(4).trim();
+        if (task.isEmpty()) {
+            throw new ToDoException();
+        }
+        return new ToDo(task);
+    }
+
+    public Task deadlineCommand() throws DeadlineException {
+        try {
+            String task = this.parameters.substring(8).trim();
+            String[] text = task.split("/by", 2);
+            String desc = text[0].trim(), by = text[1].trim();
+            if (desc.isEmpty() || by.isEmpty()) {
+                throw new DeadlineException();
+            }
+            return new Deadline(desc, by);
+        }
+        catch (Exception e) {
+            throw new DeadlineException();
+        }
+    }
+
+    public Task eventCommand() throws EventException {
+        try {
+            String task = this.parameters.substring(5).trim();
+            String[] text = task.split("/from", 2);
+            String desc = text[0].trim();
+            String[] duration = text[1].split("/to", 2);
+            String from = duration[0].trim(), to = duration[1].trim();
+            if (desc.isEmpty() || from.isEmpty() || to.isEmpty()) {
+                throw new EventException();
+            }
+            return new Event(desc, duration[0].trim(), duration[1].trim());
+        }
+        catch (Exception e) {
+            throw new EventException();
+        }
+    }
+
+    public int deleteCommand(int listSize) throws MochiException {
+        String task = this.parameters.substring(6).trim();
+        int taskNo;
+        try {
+            taskNo = Integer.parseInt(task);
+        } catch (Exception e) {
+            throw new MochiException("Please provide a task number for delete operation.");
+        }
+        if (listSize == 0) {
+            throw new MochiException("There are no tasks to delete.");
+        }
+        else if (taskNo > listSize || taskNo < 1) {
+            throw new MochiException(String.format("Invalid task number provided. Range is from 1 to %d.", listSize));
+        }
+        return taskNo;
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.util.ArrayList;
+
+// 1-indexed list
+public class TaskList {
+    private final ArrayList<Task> list;
+    private final FileHandler fh;
+
+    public TaskList(ArrayList<Task> arr, FileHandler fh) {
+        this.list = arr;
+        this.fh = fh;
+    }
+
+    public void add(Task t) {
+        list.add(t);
+        System.out.printf("""
+            ____________________________________________________________
+            Got it. I've added this task:
+                %s
+            Now you have %d tasks in  the list.
+            ____________________________________________________________
+            """, t.toString(), list.size());
+        fh.save(list);
+    }
+
+    public void remove(int taskNumber) {
+        list.remove(taskNumber - 1);
+        System.out.println("""
+            ____________________________________________________________
+            Delete operation successful.""");
+        fh.save(list);
+    }
+
+    public int size() {
+        return this.list.size();
+    }
+
+    // Returns output message
+    public String complete(int taskNumber) {
+        String res = list.get(taskNumber-1).mark();
+        fh.save(list);
+        return res;
+    }
+
+    // Returns output message
+    public String undo(int taskNumber) {
+        String res = list.get(taskNumber-1).unmark();
+        fh.save(list);
+        return res;
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        String s = """
+            ____________________________________________________________
+            Here are the tasks in your list:
+            """;
+        for (int i = 0; i < list.size(); i++) {
+            s = s.concat(String.format("%d.%s\n", i + 1, list.get(i).toString()));
+        }
+        s = s.concat("____________________________________________________________");
+        return s;
+    }
+}
