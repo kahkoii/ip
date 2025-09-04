b23d3b31af16994393a33f158ef5e408a7d033cb
Kah Ho <sweekahho@gmail.com>
2025-09-04 21:56:20 +0800

    Create Main and MainWindow classes and use FXML

--- src/main/java/mochi/Mochi.java
+++ src/main/java/mochi/Mochi.java
@@ -1,26 +1,12 @@
 package mochi;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 /**
  * Handles main Mochi application logic.
  */
-public class Mochi extends Application {
+public class Mochi {
     private final TaskList taskList;
     private final CommandParser cmd;
     private final Ui ui;
-
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/MochiMascot.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/UserIcon.png"));
 
     /**
      * Constructor for Mochi application that takes in a data file name.
@@ -35,59 +21,10 @@ public class Mochi extends Application {
     }
 
     /**
-     * Default constructor for JavaFX utility.
+     * Generates a response for the user's chat message.
      */
-    public Mochi() {
-        this("data.txt");
-    }
-
-    /**
-     * Required by JavaFX to provide concrete implementation.
-     *
-     * @param stage the primary stage that JavaFX provides
-     */
-    @Override
-    public void start(Stage stage) {
-        ScrollPane scrollPane = new ScrollPane();
-        VBox dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        TextField userInput = new TextField();
-        Button sendButton = new Button("Send");
-
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        Scene scene = new Scene(mainLayout);
-
-        // Window formatting
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        userInput.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        stage.setScene(scene);
-        stage.show();
+    public String getResponse(String input) {
+        return "Mochi heard: " + input;
     }
 
     /**
@@ -150,14 +87,5 @@ public class Mochi extends Application {
             cmd.read();
         }
         ui.exit();
-    }
-
-    /**
-     * Application main entrypoint.
-     *
-     * @param args program arguments
-     */
-    public static void main(String[] args) {
-        new Mochi("data.txt").run();
     }
 }
